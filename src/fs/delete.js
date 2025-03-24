53f4282cf4a929abff5d0841fd1af3dc3c401b02
German <maxim.p811@mail.ru>
2025-03-24 10:37:12 +0300

    delete.js upd

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,32 @@
-const remove = async () => {
-    // Write your code here 
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Получаем __dirname в ES-модулях
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const deleteFile = async () => {
+    const filePath = path.join(__dirname, 'files', 'fileToRemove.txt'); 
+
+    try {
+        await fs.access(filePath);
+        await fs.unlink(filePath);
+        console.log('File deleted successfully!');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed: File does not exist');
+        } else {
+
+            throw error;
+        }
+    }
 };
 
-await remove();
+(async () => {
+    try {
+        await deleteFile();
+    } catch (error) {
+        console.error(error.message);
+    }
+})();
