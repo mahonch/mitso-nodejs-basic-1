fcfee6fab206e3affc8986b6d8cca95d64ac2276
German <maxim.p811@mail.ru>
2025-03-24 10:38:49 +0300

    list.js upd

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,31 @@
-const list = async () => {
-    // Write your code here 
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const listFiles = async () => {
+    const dirPath = path.join(__dirname, 'files');
+
+    try {
+        await fs.access(dirPath);
+        const files = await fs.readdir(dirPath);
+        console.log('Files in the directory:');
+        files.forEach((file) => console.log(file));
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed: Directory does not exist');
+        } else {
+            throw error;
+        }
+    }
 };
 
-await list();
+(async () => {
+    try {
+        await listFiles();
+    } catch (error) {
+        console.error(error.message);
+    }
+})();
