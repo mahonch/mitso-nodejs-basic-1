c6b159faa689d821ddab46e4696071ac652d5c6d
German <maxim.p811@mail.ru>
2025-03-24 11:15:47 +0300

    read.js upd

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
-const read = async () => {
-    // Write your code here 
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, './files/fileToRead.txt'); 
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    console.log(data);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.error('File not found. Please check the path or add the file.');
+    } else {
+      throw error;
+    }
+  }
 };
 
-await read();
+readFile();
