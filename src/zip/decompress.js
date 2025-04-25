5a8a567bbf4cc52469e14331a6f36b7748bfbf45
Максим <119926184+mahonch@users.noreply.github.com>
2025-04-25 10:09:21 +0300

    Update decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+const fs = require('fs');
+const zlib = require('zlib');
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream('archive.gz');
+
+    const writeStream = fs.createWriteStream('fileToCompress.txt');
+
+    const gunzip = zlib.createGunzip();
+
+    readStream.pipe(gunzip).pipe(writeStream);
+
+    await new Promise((resolve, reject) => {
+        writeStream.on('finish', resolve);
+        writeStream.on('error', reject);
+    });
 };
 
 await decompress();
