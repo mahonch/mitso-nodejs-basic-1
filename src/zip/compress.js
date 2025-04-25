9bdc686c21e62ee1e3942afc4066cd041b079d59
Максим <119926184+mahonch@users.noreply.github.com>
2025-04-25 10:09:01 +0300

    Update compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
 const compress = async () => {
-    // Write your code here 
+    const fs = require('fs');
+    const zlib = require('zlib');
+    const { pipeline } = require('stream');
+    
+    const fileToCompress = 'files/fileToCompress.txt';
+    const outputArchive = 'archive.gz';
+
+    const gzip = zlib.createGzip();
+    const source = fs.createReadStream(fileToCompress);
+    const destination = fs.createWriteStream(outputArchive);
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+        } else {
+            console.log('File successfully compressed');
+        }
+    });
 };
 
 await compress();
